2c36f19528713ac7a86e481d4b8f1df55d4e4697
MadShark1010 <mike.paynter@envoy.com>
2023-06-27 09:36:39 -0500

    Commented out md5 and working bcrypt + salt

---
+++ app.js
@@ -0,0 +1,85 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/secretsDB", {useNewUrlParser: true});
+
+const userSchema = new Schema({
+    email: String,
+    password: String
+});
+
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User ({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save()
+            .then(() => {
+                console.log("Successfully saved User");
+                res.render("secrets");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+});
+
+app.post("/login", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username})
+        .then((foundUser) => {
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        console.log("Successfully logged in User");
+                        res.render("secrets");
+                    } else {
+                        console.log("Wrong Password!");
+                    }
+                });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
